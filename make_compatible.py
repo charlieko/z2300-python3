db7aa6c18576ccae6aac63ea45442820827dd605
Charles Ko <charlieko@users.noreply.github.com>
2023-01-22 16:09:24 -0800

    Make it compatible with python3 on windows
    Fixing print syntax and replacing pyexiv2 with piexif which does not require c++ library.

--- make_compatible.py
+++ make_compatible.py
@@ -1,7 +1,7 @@
 import PIL
 import PIL.Image
 
-import pyexiv2
+import piexif
 
 import os
 import shutil
@@ -15,7 +15,7 @@ def rename(f):
   while True:
     nf = "PICT%04d.JPG" % random.randint(1111,9999)
     if not os.path.isfile(nf):
-      print f,"->",nf
+      print(f,"->",nf)
       return nf
 
 # Fix image paths
@@ -27,13 +27,13 @@ for f in os.listdir('.'):
       
 for f in os.listdir('.'):
   if f.upper().endswith('JPG'):
-    print "Checking", f
+    print("Checking", f)
     # Find the image size
     im = PIL.Image.open(f)
     im_size = im.size
   
     if im_size != (3648,2736) and im_size != (2736,3648):
-      print "Resizing image to POLAROID dimensions"
+      print("Resizing image to POLAROID dimensions")
       im = im.resize((im_size[0] * 4, im_size[1] * 4))
       if im_size[0] > im_size[1]:
         into = PIL.Image.new(im.mode, (3648,2736))
@@ -46,27 +46,27 @@ for f in os.listdir('.'):
       im = into
   
     im_size = im.size
-    
-    del im
 
-    source_image = pyexiv2.ImageMetadata(f)
-    source_image.read()
+    source_image = piexif.load(im.info["exif"])
+    print(source_image)
     
     try:
-      exif_width = int(source_image["Exif.Photo.PixelXDimension"].raw_value)
-      exif_height = int(source_image["Exif.Photo.PixelYDimension"].raw_value)
-    except KeyError:
+      exif_width = source_image["Exif"][piexif.ExifIFD.PixelXDimension]
+      exif_height = source_image["Exif"][piexif.ExifIFD.PixelYDimension]
+    except IndexError:
       exif_width = None
       exif_height = None
 
-#    for k,v in source_image.items():
-#      print k,v
+    for k,v in source_image.items():
+      print(k,v)
     
     if exif_width != im_size[0] or exif_height != im_size[1]:
-      print "EXIF data was bad for %s" % f
-      print "  width  - ", exif_width, im_size[0]
-      print "  height - ", exif_height, im_size[1]
-      
-      source_image["Exif.Photo.PixelXDimension"] = im_size[0]
-      source_image["Exif.Photo.PixelYDimension"] = im_size[1]
-      source_image.write()
+      print("EXIF data was bad for %s" % f)
+      print("  width  - ", exif_width, im_size[0])
+      print("  height - ", exif_height, im_size[1])
+
+      source_image["Exif"][piexif.ExifIFD.PixelXDimension] = im_size[0]
+      source_image["Exif"][piexif.ExifIFD.PixelYDimension] = im_size[1]
+      print(source_image)
+      exif_bytes = piexif.dump(source_image)
+      im.save(f, "jpeg", exif=exif_bytes)
